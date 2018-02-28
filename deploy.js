fe3a10b6815b8c4552e14b8d02f7001916d87d5d
Du En Yang <377153400@qq.com>
2018-02-28 16:53:24 +0800

    dede

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 var http = require('http')
 var createHandler = require('github-webhook-handler')
 var handler = createHandler({
-    path: '/',
+    path: '/webhook',
     secret: 'duenyang'
 })
 // 上面的 secret 保持和 GitHub 后台设置的一致
